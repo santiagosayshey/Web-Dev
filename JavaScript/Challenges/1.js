e3526e6625b18d222032b77b4725e794ba0e3bcf
santiagosayshey <chaucted@gmail.com>
2023-01-08 09:51:03 +1030

    added BMI function!

--- JavaScript/Challenges/1.js
+++ JavaScript/Challenges/1.js
@@ -1,14 +1,18 @@
 /* Coding Challenge 1 */
 
+function BMI(height, mass) {
+  return mass / height ** 2;
+}
+
 const markHeight = 1.88;
 const markMass = 95;
 
 const johnHeight = 1.76;
 const johnMass = 85;
 
-const mBMI = markMass / markHeight ** 2;
+const mBMI = BMI(markHeight, markMass);
 
-const jBMI = johnMass / johnHeight ** 2;
+const jBMI = BMI(johnHeight, johnMass);
 
 const markHigherBMI = mBMI > jBMI;
 
