7cde5a8a0860adc8234061670f60fbfd4e5545f9
santiagosayshey <chaucted@gmail.com>
2023-01-08 08:18:26 +1030

    finished assignment 1

---
+++ JavaScript/Assignments/script.js
@@ -0,0 +1,14 @@
+/* Values and Variables */
+let country = "Australia";
+let continent = country;
+let population = 22000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/* Data Types */
+let isIsland = true;
+let lan;
+
+let age = 23;
---
+++ JavaScript/Exercises/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "jonas";
+
+console.log(firstName);
