49ea38701aec85754259aa03801dd62106537992
santiagosayshey <chaucted@gmail.com>
2023-01-08 11:49:18 +1030

    finsihed strings and if else statements

--- JavaScript/Exercises/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript/Exercises/01-Fundamentals-Part-1/starter/script.js
@@ -17,7 +17,7 @@ let b;
 console.log(b);
 console.log(typeof b);
  */
-
+/* 
 const year = 2099;
 
 const ageJonas = year - 1991;
@@ -33,3 +33,34 @@ console.log(name1 + name2);
 
 const x = 5 + 50;
 console.log(x);
+ */
+
+/* const firstName = "Sam";
+const job = "Student";
+const birthYear = 2001;
+const year = 2023;
+
+const sam =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job;
+
+const samNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+
+console.log(samNew);
+
+console.log(`yo
+wassup
+my
+name
+is
+sam`);
+ */
+
+const age = 1;
+
+if (age === 19) {
+  console.log("yeah im 19");
+} else if (age === 17) {
+  console.log("no im not 19");
+} else {
+  console.log("go away");
+}
